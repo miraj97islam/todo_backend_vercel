5ce34f18b72086b4abb5ab9a8ddb0341e343657a
MD MIRAJ ISLAM <miraz97islam@gmail.com>
2025-01-07 23:31:26 +0600

    SetHeader
    --SetHeader to https://todo-backend-vercel-cyan.vercel.app

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const server = http.createServer((req, res) =>{
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://todo-backend-vercel-cyan.vercel.app');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
 
